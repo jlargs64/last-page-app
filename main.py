43552ac5abd4ff06e25a651e56f6a39d585c9257
Justin Largo <justinlargo7@gmail.com>
2025-10-23 20:48:51 -0400

    Added the ability to create epubs :)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from typing import Annotated
-import tempfile
-from pathlib import Path
-from fastapi import FastAPI, File
-from ebooklib import epub
-from fastapi.responses import FileResponse
+import os
 import uuid
+from typing import Annotated
+
+from ebooklib import epub
+from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.responses import FileResponse
+from starlette.background import BackgroundTask
 
 app = FastAPI()
 
@@ -19,17 +20,19 @@ async def health():
     return {"status": "OK"}
 
 
-@app.post("/create-cover")
-async def create_cover(cover_image: Annotated[bytes, File()], author: str):
+@app.post("/api/create-cover")
+async def create_cover(
+    cover_image: Annotated[UploadFile, File()], author: Annotated[str, Form()]
+):
+    cover_image_bytes = await cover_image.read()
     book = epub.EpubBook()
-    book.set_identifier(uuid.uuid1())
+    file_id = str(uuid.uuid1())
+    book.set_identifier(file_id)
     book.set_title(f"{author}'s cover")
     book.add_author(author)
     book.set_language("en")
-    with tempfile.NamedTemporaryFile(delete=True, suffix="png") as tmp:
-        tmp.write(cover_image)
-        tmp_path = tmp.name
-        book.set_cover(f"{author}_cover.png", cover_image)
-
-        # return FileResponse()
-        return {"ok": "ok"}
+    book.set_cover(f"{author}_cover.png", cover_image_bytes)
+    book.spine = ["cover"]
+    epub_path = f"{file_id}.epub"
+    epub.write_epub(epub_path, book, {})
+    return FileResponse(epub_path, background=BackgroundTask(os.unlink, epub_path))
